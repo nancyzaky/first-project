9d205bc40997222c038dd045f721eb9a74ffe19b
Nancy Zaky <nancyzki@yahoo.com>
2021-05-28 19:54:47 -0700

    styling

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-const h2 = document.createElement('h2');
-h2.textContent = "this is my first project";
-
-
-document.querySelector('body').appendChild(h2)
+let butt = document.querySelector('order-btn');
